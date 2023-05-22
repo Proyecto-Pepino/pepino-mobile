c09ea5bcbc557d73d7b9342d314e34b69c0c65a6
mariano <cirilliluis@gmail.com>
2023-05-22 19:13:40 -0300

    product logic

--- components/product.js
+++ components/product.js
@@ -1,19 +1,54 @@
 import { useNavigation } from "@react-navigation/native";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Entypo, AntDesign } from "@expo/vector-icons";
 import {
   TouchableOpacity,
   Text,
   StyleSheet,
   View,
   TextInput,
-  ScrollView
+  ScrollView,
+  Button,
 } from "react-native";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
 import { MaterialIcons } from "@expo/vector-icons";
 
 const Product = ({ route }) => {
   const { productData } = route.params;
+  
+  const [sellData, setSellData] = useState({
+    amount: 1,
+    price: productData.price,
+  });
+  const [priceAmount, setPriceAmount] = useState(productData.price);
 
+  const HandlerSellAdd = () => {
+    setSellData({ ...sellData, amount: sellData.amount + 1 });
+  };
+
+  const HandlerSellSustract = () => {
+    if (sellData.amount > 0) {
+      setSellData({ ...sellData, amount: sellData.amount - 1 });
+    }
+  };
+
+  const handlePrice = (amount, price) => {
+    
+    let result = amount * price;
+
+    return result;
+  };
+
+  useEffect(() => {
+    
+
+    setPriceAmount(handlePrice(sellData.amount, sellData.price));
+    console.log(priceAmount)
+  }, [sellData]);
+
+  const handleButton = () => {
+    console.log("Button Press");
+  };
   const navigation = useNavigation();
   return (
     <ScrollView style={stylesProduct.container}>
@@ -42,9 +77,43 @@ const Product = ({ route }) => {
           multiline={true}
           numberOfLines={6}
           style={stylesProduct.inputProduct}
-          
-  placeholderTextColor="#bfbfbf"
+          placeholderTextColor="#bfbfbf"
         ></TextInput>
+      </View>
+
+      <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+        <View style={stylesProduct.addQuantity}>
+          <AntDesign
+            name="minus"
+            size={24}
+            color="black"
+            onPress={HandlerSellSustract}
+          />
+          <Text style={{ marginLeft: 20, marginRight: 20 }}>
+            {sellData.amount}
+          </Text>
+          <Entypo
+            name="plus"
+            size={24}
+            color="black"
+            onPress={HandlerSellAdd}
+          />
+        </View>
+        <View style={{ width: "60%" }}>
+          <View style={{ marginTop: 22 }}>
+            <TouchableOpacity
+              title={` Agregar  ${priceAmount}`}
+              disabled
+              onPress={() => {
+                handleButton();
+              }}
+              style={stylesProduct.buttonAdd}
+            >
+              <Text style={{ color: "white" }}>Agregar </Text>
+              <Text style={{ color: "#ffffff" }}>$ {priceAmount}</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
     </ScrollView>
   );
@@ -94,5 +163,25 @@ const stylesProduct = StyleSheet.create({
     borderWidth: 1,
     borderColor: "#8F8C8C",
     borderRadius: 10,
+  },
+  addQuantity: {
+    alignSelf: "flex-start",
+    borderColor: "black",
+    padding: 10,
+    height: 50,
+    flexDirection: "row",
+    marginTop: 22,
+    borderWidth: 0.8,
+    borderRadius: 10,
+  },
+  buttonAdd: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    borderRadius: 20,
+    height: 50,
+    backgroundColor: "#A9A7A7",
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
--- redux/productsSlice.js
+++ redux/productsSlice.js
@@ -3,14 +3,14 @@ import {  createSlice } from '@reduxjs/toolkit';
 const ProductSlice = createSlice({
     name: 'Products',
     initialState: {fruits: [
-        {id:1, name:'Manzana Roja',price:`$ ${450}`},
-        {id:2, name:'Naranja',price:`$ ${250}`},
-        {id:3, name:'Pera' ,price:`$ ${500}`},
-        {id:4, name:'Tomate' ,price:`$ ${550}`},
-        {id:5, name:'Zanahoria',price:`$ ${670}`},
-        {id:6, name:'Ananá' ,price:`$ ${500}`},
-        {id:7, name:'Lechuga' ,price:`$ ${550}`},
-        {id:8, name:'Pepino',price:`$ ${670}`},
+        {id:1, name:'Manzana Roja',price:450},
+        {id:2, name:'Naranja',price:250},
+        {id:3, name:'Pera' ,price:500},
+        {id:4, name:'Tomate' ,price:550},
+        {id:5, name:'Zanahoria',price:670},
+        {id:6, name:'Ananá' ,price:500},
+        {id:7, name:'Lechuga' ,price:550},
+        {id:8, name:'Pepino',price:670},
     ]
   },
 
